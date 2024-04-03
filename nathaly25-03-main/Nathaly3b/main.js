2393b8d971c895f24163ac1f8fd0034ae4762d91
nathaly3245 <127852587+nathaly3245@users.noreply.github.com>
2024-04-03 09:07:59 -0300

    Add files via upload

--- nathaly25-03-main/Nathaly3b/main.js
+++ nathaly25-03-main/Nathaly3b/main.js
@@ -21,15 +21,21 @@ const tempoObjetivo4 = new Date ("2024-02-01T00:00:00");
  
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-function calculaTempo(tempoObjetico){
+function calculaTempo(tempoObjetivo){
     let tempoAtual = newDate();
     let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24);
+    let segundos = Math.floor (tempoFinal /1000);
+    let minutos = Math.floor (segundos /60);
+    let horas = Math.floor (minutos /60);
+    let dias = Math.floor (horas /24);
     
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
+
+    if (tempoFinal >0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    }else{ 
+        return "Prazo Finalizado";
+    }
 }
